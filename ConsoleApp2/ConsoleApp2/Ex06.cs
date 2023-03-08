e265daeca10a0822f81ecdd113ad901ab2ce6dd1
mrgamerads <66223274+mrgamerads@users.noreply.github.com>
2023-03-08 14:38:51 -0300

    TP LP1 Ex03,06 e 07

---
+++ ConsoleApp2/ConsoleApp2/Ex06.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Ex06
+    {
+        private static void main(string[] args)
+        {
+            int p1, p2, media;
+
+            Console.WriteLine("Digite a nota da P1: ");
+            p1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da P2: ");
+            p2 = Convert.ToInt32(Console.ReadLine());
+
+            media = p1 + p2 * 2 / 3;
+
+            Console.WriteLine("A média total do Aluno é: " + media);
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double diag1, diag2, diag3, diag4;
+        double diagTot;
+
+        Console.WriteLine("Digite o valor da Diagonal");
+        diag1 = Convert.ToDouble(Console.ReadLine());
+
+        diag2 = diag1;
+        diag3 = diag2;
+        diag4 = diag3;
+
+        diagTot = diag1 + diag2 + diag3 +diag4;
+
+        Console.WriteLine("O valor total do quadrado é de: " + diagTot);
+
+
+    }
+
+internal class Ex06
+    {
+        private static void Main(string[] args)
+        {
+            int p1, p2, media, mediaFinal;
+
+            Console.WriteLine("Digite a nota da P1: ");
+            p1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota da P2: ");
+            p2 = Convert.ToInt32(Console.ReadLine());
+
+            media = p1 + p2 * 2;
+
+            mediaFinal = media / 3;
+
+            Console.WriteLine("A média total do Aluno é: " + mediaFinal);
+            
+            if (mediaFinal >= 7) {
+            Console.WriteLine("Aluno Aprovado!");
+            }
+            else
+            {
+            Console.WriteLine("Aluno Reprovado");
+            }
+        
+
+        }
+    }
+*/
+
+internal class Ex07
+{
+    private static void Main(string[] args)
+    {
+        int medGeo;
+
+        Console.WriteLine("Digite um valor: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite outro valor: ");
+        int b = int.Parse(Console.ReadLine());
+
+        medGeo = (int)Math.Sqrt(a * a + b * b);
+
+        Console.WriteLine("A media geografica dos valores é de: " + medGeo);
+
+    }
+
+}
